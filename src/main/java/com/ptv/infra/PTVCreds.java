8c4f2d58e1d6be764bfadee71a44970dfe2846b7
rogueagent <piyushbhawan@gmail.com>
2017-10-10 01:13:50 +1100

    ewmoved file from tracking

--- src/main/java/com/ptv/infra/PTVCreds.java
+++
@@ -1,9 +0,0 @@
-package com.ptv.infra;
-
-public class PTVCreds {
-	public static final String devId = "";
-	
-	public static final String privateKey= "";
-	
-	public static final String baseUrl = "https://timetableapi.ptv.vic.gov.au";
-}
