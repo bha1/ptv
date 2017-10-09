526e0fdd0e880b6576d180f850bf84bba36fffab
rogueagent <piyushbhawan@gmail.com>
2017-10-10 01:06:11 +1100

    doing a sandboxing for connection with ptv

---
+++ src/main/java/com/ptv/infra/PTVCreds.java
@@ -0,0 +1,9 @@
+package com.ptv.infra;
+
+public class PTVCreds {
+	public static final String devId = "";
+	
+	public static final String privateKey= "";
+	
+	public static final String baseUrl = "https://timetableapi.ptv.vic.gov.au";
+}
