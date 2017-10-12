5804c93625bfe50eb614ceef4dfed93285ef3873
rogueagent <piyushbhawan@gmail.com>
2017-10-12 23:40:50 +1100

    health check

---
+++ src/main/java/com/example/HealthCheck.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(path = "/healthcheck")
+public class HealthCheck {
+
+
+	@RequestMapping(path = "/a",method = RequestMethod.GET)
+	public String check() {
+		int timeout = 1000;
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
+        SimpleClientHttpRequestFactory rf = (SimpleClientHttpRequestFactory) restTemplate
+                .getRequestFactory();
+        rf.setReadTimeout(timeout);
+        rf.setConnectTimeout(timeout);
+        String quote = null;
+        try {
+			Thread.sleep(1600000);
+			quote = restTemplate.getForObject("http://127.0.0.1:5000/healthcheck/b", String.class);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.err.println("time out in health a");
+		}
+        System.out.println(quote+"->a");
+		return quote;
+	}
+	
+	@RequestMapping(path = "/b",method = RequestMethod.GET)
+	public String checkb() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
+        SimpleClientHttpRequestFactory rf = (SimpleClientHttpRequestFactory) restTemplate
+                .getRequestFactory();
+        rf.setReadTimeout(1000);
+        rf.setConnectTimeout(1000);
+        String quote = null;
+        try {
+ 			Thread.sleep(1600000);
+ 			quote = restTemplate.getForObject("http://127.0.0.1:5000/healthcheck/a", String.class);
+ 		} catch (Exception e) {
+ 			// TODO Auto-generated catch block
+ 			System.err.println("time out in health b");
+ 		}
+        System.out.println(quote+"->b");
+        return quote;
+	}
+	
+}
